04bd48261c4f9eec0c3d03eea432f4b74f8eba26
KyleHEng <kylehussaineng@gmail.com>
2025-03-04 11:18:32 +0000

    "Add achievement_unlocked to achievements data and table"

--- db/data/testData/achievements.ts
+++ db/data/testData/achievements.ts
@@ -2,9 +2,11 @@ module.exports = [
   {
     achievement: "5 words learned!",
     user_id: 1,
+    achievement_unlocked: true,
   },
   {
     achievement: "10 words learned!",
     user_id: 1,
+    achievement_unlocked: false,
   },
 ];
