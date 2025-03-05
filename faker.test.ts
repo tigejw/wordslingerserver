3e613fdda5826a626966a7f59d9eb866e5857787
artiechokeheart <artemiswilkinson@hotmail.com>
2025-03-05 16:09:46 +0000

    update dev & test data

--- faker.test.ts
+++ faker.test.ts
@@ -1,20 +1,20 @@
-import {
-  createFriendsData,
-  createGameData,
-  createLeaderboardData,
-  createUsersData,
-  createUsersLanguagesData,
-  createWordMasteryData,
-  dataGeneration,
-} from "./db/data/faker";
+// import {
+//   createFriendsData,
+//   createGameData,
+//   createLeaderboardData,
+//   createUsersData,
+//   createUsersLanguagesData,
+//   createWordMasteryData,
+//   dataGeneration,
+// } from "./db/data/faker";
 
 describe("testing", () => {
   test("testing", () => {});
 
-  // console.log(createGameData(1));
-  console.log(dataGeneration());
-  // console.log(createFriendsData(1));
-  // console.log(createUsersLanguagesData(1));
-  // console.log(createLeaderboardData(1));
-  // console.log(createWordMasteryData(1));
+  //   // console.log(createGameData(1));
+  //   console.log(dataGeneration());
+  //   // console.log(createFriendsData(1));
+  //   // console.log(createUsersLanguagesData(1));
+  //   // console.log(createLeaderboardData(1));
+  //   // console.log(createWordMasteryData(1));
 });
