5414fff2180478b137f95224ec5343ec70d101ba
artiechokeheart <artemiswilkinson@hotmail.com>
2025-03-05 09:52:26 +0000

    Made amendments to the seed re: Types and start functions for faker

--- types.ts
+++ types.ts
@@ -8,7 +8,7 @@ export type User = {
 };
 
 export type Language = {
-  username: string;
+  user_id: number;
   language: "German" | "Spanish" | "French";
   current_level: number;
 };
@@ -19,4 +19,46 @@ export type Word = {
   german: string;
   spanish: string;
   word_level: number;
+};
+
+export type Data = {
+  achievementsData: Array<Achievement>;
+  friendsData: Array<Friend>;
+  gamesData: Array<Game>;
+  languagesData: Array<Language>;
+  leaderboardData: Array<Leaderboard>;
+  usersData: Array<User>;
+  word_masteryData: Array<WordMastery>;
+  wordsData: Array<Word>;
+};
+
+export type Game = {
+  game: string;
+  winner: number | null;
+  loser: number | null;
+  isDraw: boolean;
+};
+
+export type Achievement = {
+  achievement: string;
+  user_id: number;
+};
+
+export type Leaderboard = {
+  rank: number;
+  user_id: number;
+  language: string;
+};
+
+export type Friend = {
+  user_id1: number;
+  user_id2: number;
+  status: string;
+};
+
+export type WordMastery = {
+  english: string;
+  german_mastery: "beginner" | "intermediate" | "master";
+  spanish_mastery: string;
+  french_mastery: string;
 };
