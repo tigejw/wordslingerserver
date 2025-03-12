b786bd766bc2d1f2ff7a6500d6287601c120e2d5
KyleHEng <kylehussaineng@gmail.com>
2025-03-12 13:13:51 +0000

    Add patch for word mastery

--- api/__tests/review-api.test.ts
+++ api/__tests/review-api.test.ts
@@ -1,4 +1,4 @@
-import { ReviewData } from "@/types";
+import { ReviewData, UpdatedMastery } from "@/types";
 
 const request = require("supertest");
 const app = require("../index");
@@ -16,24 +16,86 @@ afterAll(() => {
 
 type ReviewResponse = { body: { reviewData: ReviewData } };
 type ErrorResponse = { body: { error: string } };
+type UpdatedMasteryResponse = { body: { updatedMastery: UpdatedMastery } };
 
-describe("/reviews/:user_id", () => {
-  test("GET /reviews/:user_id", () => {
-    return request(app)
-      .get("/api/reviews/1")
-      .expect(200)
-      .then(({ body: { reviewData } }: ReviewResponse) => {
-        expect(Array.isArray(reviewData.frenchReviewData)).toBe(true);
-        expect(Array.isArray(reviewData.germanReviewData)).toBe(true);
-        expect(Array.isArray(reviewData.spanishReviewData)).toBe(true);
-      });
+describe("/api/reviews/:user_id", () => {
+  describe("GET:", () => {
+    test("200: /api/reviews/:user_id", () => {
+      return request(app)
+        .get("/api/reviews/1")
+        .expect(200)
+        .then(({ body: { reviewData } }: ReviewResponse) => {
+          expect(Array.isArray(reviewData.frenchReviewData)).toBe(true);
+          expect(Array.isArray(reviewData.germanReviewData)).toBe(true);
+          expect(Array.isArray(reviewData.spanishReviewData)).toBe(true);
+        });
+    });
+    test("404: Responds with an error if user_id does not exist", () => {
+      return request(app)
+        .get("/api/reviews/100000")
+        .expect(404)
+        .then(({ body: { error } }: ErrorResponse) => {
+          expect(error).toEqual("Not found!");
+        });
+    });
   });
-  test("404: Responds with an error if user_id does not exist", () => {
-    return request(app)
-      .get("/api/reviews/100000")
-      .expect(404)
-      .then(({ body: { error } }: ErrorResponse) => {
-        expect(error).toEqual("Not found!");
-      });
+  describe("PATCH: /api/reviews/:user_id", () => {
+    test("should respond wtih 200 and updated mastery when sent with correct body", () => {
+      return request(app)
+        .patch("/api/reviews/1")
+        .send({
+          english: "cat",
+          target_language: "german",
+          new_mastery: "intermediate",
+        })
+        .expect(200)
+        .then(({ body: { updatedMastery } }: UpdatedMasteryResponse) => {
+          const { user_id, english, german_mastery } = updatedMastery;
+          expect(user_id).toBe(1);
+          expect(english).toBe("cat");
+          expect(german_mastery).toBe("intermediate");
+        });
+    });
+  });
+  describe("should respond with 400 and error message when sent malformed body", () => {
+    test("eng", () => {
+      return request(app)
+        .patch("/api/reviews/1")
+        .send({
+          eng: "cat",
+          target_language: "german",
+          new_mastery: "intermediate",
+        })
+        .expect(400)
+        .then(({ body: { error } }: ErrorResponse) => {
+          expect(error).toBe("Bad request!");
+        });
+    });
+    test("targnguage", () => {
+      return request(app)
+        .patch("/api/reviews/1")
+        .send({
+          english: "cat",
+          targnguage: "german",
+          new_mastery: "intermediate",
+        })
+        .expect(400)
+        .then(({ body: { error } }: ErrorResponse) => {
+          expect(error).toBe("Bad request!");
+        });
+    });
+    test("nestery", () => {
+      return request(app)
+        .patch("/api/reviews/1")
+        .send({
+          english: "cat",
+          target_language: "german",
+          nestery: "intermediate",
+        })
+        .expect(400)
+        .then(({ body: { error } }: ErrorResponse) => {
+          expect(error).toBe("Bad request!");
+        });
+    });
   });
 });
